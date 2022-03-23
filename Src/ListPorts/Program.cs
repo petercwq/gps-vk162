e21f78b120e0477617a6b291e7d966d48d62f572
Peter Chen <peter_chen@trimble.com>
2022-03-23 14:50:07 -0600

    Read serial port names

---
+++ Src/ListPorts/Program.cs
@@ -0,0 +1,11 @@
+﻿// Get a list of serial port names.
+using System.IO.Ports;
+
+string[] ports = SerialPort.GetPortNames();
+
+Console.WriteLine("The following serial ports were found:");
+// Display each port name to the console.
+foreach (string port in ports)
+{
+    Console.WriteLine(port);
+}
