30142b914bc710963695c3f52cf9f783731bb275
Peter Chen <peter_chen@trimble.com>
2022-03-23 15:00:45 -0600

    Read line from serial port

--- Src/ReadNmea/Program.cs
+++ Src/ReadNmea/Program.cs
@@ -1,2 +1,8 @@
-﻿
-Console.ReadLine();
+﻿using System.IO.Ports;
+
+using var sp = new SerialPort("/dev/ttyACM0", 115200);
+while (true)
+{
+    var line = sp.ReadLine();
+    Console.WriteLine(line);
+}
